26c6af1c0def2a02ea52470c30e19ef49b01ef0b
mschijf <mschijf@gmail.com>
2022-12-03 16:08:09 +0100

    remove warnings

--- src/com/adventofcode/mylambdas/MyCollectionLambdas.kt
+++ src/com/adventofcode/mylambdas/MyCollectionLambdas.kt
@@ -1,12 +1,12 @@
 package com.adventofcode.mylambdas
 
-public fun <T> Iterable<T>.splitByCondition(predicate: (T) -> Boolean): List<List<T>> {
+fun <T> Iterable<T>.splitByCondition(predicate: (T) -> Boolean): List<List<T>> {
     val result = ArrayList<List<T>>()
     var tmp = mutableListOf<T>()
     this.forEach {
         if (predicate(it)) {
             result.add(tmp)
-            tmp = mutableListOf<T>()
+            tmp = mutableListOf()
         } else {
             tmp.add(it)
         }
