2842e7445c45f912a6f9d538f3033f48a721dea1
mschijf <mschijf@gmail.com>
2022-12-04 14:38:36 +0100

    refactor once more - data georganiseerd

--- src/com/adventofcode/Input.kt
+++ src/com/adventofcode/Input.kt
@@ -3,8 +3,9 @@ package com.adventofcode
 import java.io.File
 
 class Input(test: Boolean, dayOfMonth: Int) {
-    private val path = "data"
-    private val fileName = "input" + String.format("%02d", dayOfMonth) + "_" + if (test) "example" else "1"
+    private val path = "data/december${String.format("%02d", dayOfMonth)}"
+    private val fileName = if (test) "example" else "input"
     private val file = File("$path/$fileName")
+
     val inputLines = if (file.exists()) file.bufferedReader().readLines() else emptyList()
 }
