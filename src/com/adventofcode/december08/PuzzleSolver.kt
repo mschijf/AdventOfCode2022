7479d2fe65e3cccec78b5c106b191fbd1547d492
mschijf <mschijf@gmail.com>
2022-12-08 08:11:23 +0100

    adding December08 - refactored

--- src/com/adventofcode/december08/PuzzleSolver.kt
+++ src/com/adventofcode/december08/PuzzleSolver.kt
@@ -7,111 +7,56 @@ fun main() {
 }
 
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
-    private val grid = input.inputLines.map { it -> it.toList().map{Info(it - '0', 0)} }
+    private val grid = input.inputLines.map { it -> it.toList().map { it - '0' } }
 
     override fun resultPartOne(): String {
-        countVisibleFromLeft()
-        countVisibleFromRight()
-        countVisibleFromTop()
-        countVisibleFromBottom()
         return grid
-            .sumOf { it -> it.sumOf { it.second } }
+            .mapIndexed { rowIndex, row -> List(row.size) { colIndex -> isVisibleTree(rowIndex, colIndex) } }
+            .sumOf { it.count { visibleTree -> visibleTree } }
             .toString()
     }
 
     override fun resultPartTwo(): String {
         return grid
             .mapIndexed { rowIndex, row -> List(row.size) { colIndex -> scenicValue(rowIndex, colIndex) } }
-            .maxOfOrNull { it.max() }
+            .maxOf { it.max() }
             .toString()
     }
 
-    private fun scenicValue(row: Int, col: Int) = lookDown(row, col) * lookUp(row, col) * lookLeft(row, col) * lookRight(row, col)
+    private fun isVisibleTree(row: Int, col: Int) =
+        isVisibleTree(row, col, 0, 1) ||
+                isVisibleTree(row, col, 0, -1) ||
+                isVisibleTree(row, col, 1, 0) ||
+                isVisibleTree(row, col, -1, 0)
 
-    private fun countVisibleFromLeft() {
-        for (row in grid.indices) {
-            var heighest = -1
-            for (col in 0 until grid[row].size) {
-                if (grid[row][col].first > heighest) {
-                    grid[row][col].second = 1
-                    heighest = grid[row][col].first
-                }
-            }
+    private fun scenicValue(row: Int, col: Int) =
+        countVisible(row, col, 0, 1) *
+                countVisible(row, col, 0, -1) *
+                countVisible(row, col, 1, 0) *
+                countVisible(row, col, -1, 0)
+
+    private fun isVisibleTree(row: Int, col: Int, rowDir: Int, colDir: Int): Boolean {
+        var newRow = row + rowDir
+        var newCol = col + colDir
+        while (newRow >= 0 && newCol >= 0 && newRow < grid.size && newCol < grid[newRow].size) {
+            if (grid[newRow][newCol] >= grid[row][col])
+                return false
+            newRow += rowDir
+            newCol += colDir
         }
+        return true
     }
 
-    private fun countVisibleFromRight() {
-        for (row in grid.indices) {
-            var heighest = -1
-            for (col in grid[row].size - 1 downTo 0) {
-                if (grid[row][col].first > heighest) {
-                    grid[row][col].second = 1
-                    heighest = grid[row][col].first
-                }
-            }
-        }
-    }
-
-    private fun countVisibleFromTop() {
-        for (col in 0 until grid[0].size) {
-            var heighest = -1
-            for (row in grid.indices) {
-                if (grid[row][col].first > heighest) {
-                    grid[row][col].second = 1
-                    heighest = grid[row][col].first
-                }
-            }
-        }
-    }
-
-    private fun countVisibleFromBottom() {
-        for (col in 0 until grid[0].size) {
-            var heighest = -1
-            for (row in grid.size-1 downTo 0) {
-                if (grid[row][col].first > heighest) {
-                    grid[row][col].second = 1
-                    heighest = grid[row][col].first
-                }
-            }
-        }
-    }
-
-    private fun lookRight(row: Int, col: Int): Int {
+    private fun countVisible(row: Int, col: Int, rowDir: Int, colDir: Int): Int {
         var result = 0
-        for (i in col-1 downTo 0) {
+        var newRow = row + rowDir
+        var newCol = col + colDir
+        while (newRow >= 0 && newCol >= 0 && newRow < grid.size && newCol < grid[newRow].size) {
             result++
-            if (grid[row][i].first >= grid[row][col].first)
+            if (grid[newRow][newCol] >= grid[row][col])
                 return result
-        }
-        return result
-    }
-
-    private fun lookLeft(row: Int, col: Int): Int {
-        var result = 0
-        for (i in col+1 until grid[row].size) {
-            result++
-            if (grid[row][i].first >= grid[row][col].first)
-                return result
-        }
-        return result
-    }
-
-    private fun lookUp(row: Int, col: Int): Int {
-        var result = 0
-        for (i in row-1 downTo 0) {
-            result++
-            if (grid[i][col].first >= grid[row][col].first)
-                return result
-        }
-        return result
-    }
-
-    private fun lookDown(row: Int, col: Int): Int {
-        var result = 0
-        for (i in row+1 until grid.size) {
-            result++
-            if (grid[i][col].first >= grid[row][col].first)
-                return result
+            newRow += rowDir
+            newCol += colDir
         }
         return result
     }
@@ -119,4 +64,3 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
 //----------------------------------------------------------------------------------------------------------------------
 
-class Info(var first: Int, var second: Int)
