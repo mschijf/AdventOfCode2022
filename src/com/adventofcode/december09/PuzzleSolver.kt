a98335fd5fb45e405ffd1fe8c7dc372007342a4b
mschijf <mschijf@gmail.com>
2022-12-09 20:52:23 +0100

    December09 - refactored

--- src/com/adventofcode/december09/PuzzleSolver.kt
+++ src/com/adventofcode/december09/PuzzleSolver.kt
@@ -2,6 +2,7 @@ package com.adventofcode.december09
 
 import com.adventofcode.PuzzleSolverAbstract
 import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 fun main() {
     PuzzleSolver(test=false).showResult()
@@ -10,81 +11,49 @@ fun main() {
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
     override fun resultPartOne(): String {
-        var head = Position(0,0)
-        var tail = Position(0,0)
-        val tailSet = mutableSetOf<Position>(tail)
-        input.inputLines
-            .map{createMoveList(it)}
-            .flatten()
-            .forEach {mv ->
-            head = head.move(mv)
-            if (head.distanceTooFar(tail)) {
-                tail = tail.follow(head, mv)
-                tailSet.add(tail)
-//                println(tail)
-            }
-        }
-        return tailSet.count().toString()
+        return doSolve(2).toString()
     }
 
     override fun resultPartTwo(): String {
-        var knots = MutableList(10) {Position(0,0) }
-        val tailSet = mutableSetOf<Position>(knots.last())
+        return doSolve(10).toString()
+    }
+
+    private fun doSolve(numberOfKnots: Int): Int {
+        val knots = MutableList(numberOfKnots) { Position(0,0) }
+        val tailSet = mutableSetOf(knots.last())
         input.inputLines
-            .map{createMoveList(it)}
+            .map{ line -> List(line.substring(2).toInt()) { Move(line[0]) } }
             .flatten()
-            .forEach {mv ->
+            .forEach { mv ->
                 knots[0] = knots[0].move(mv)
                 for (i in 1 until knots.size) {
                     if (knots[i-1].distanceTooFar(knots[i])) {
-                        knots[i] = knots[i].follow(knots[i-1], mv)
-                    } else {
-                        break
+                        knots[i] = knots[i].follow(knots[i-1])
                     }
                 }
                 tailSet.add(knots.last())
             }
-        return tailSet.count().toString()
-    }
-
-    fun createMoveList(s: String): List<Move> {
-        val result = mutableListOf<Move>()
-        val split =s.split(" ")
-        val stepSize = split[1].toInt()
-        repeat(stepSize) {
-            result.add(Move(split[0]))
-        }
-        return result
+        return tailSet.count()
     }
 }
-
-
 
 //----------------------------------------------------------------------------------------------------------------------
 
 data class Position (val x: Int, val y: Int) {
     fun move(mv:Move) = Position(this.x+mv.dirX, y+mv.dirY)
     fun distanceTooFar(other: Position) = (this.x - other.x).absoluteValue > 1 || (this.y - other.y).absoluteValue > 1
-
-    fun follow(other: Position, mv: Move): Position {
-        if (x == other.x)
-            return Position(x, y  + if (y < other.y) 1 else if (y > other.y) -1 else 0)
-        else if (y == other.y)
-            return Position(x  + if (x < other.x) 1 else if (x > other.x) -1 else 0, y)
-        else
-            return Position(x + if (x < other.x) 1 else -1, y + if (y < other.y) 1 else -1)
-    }
+    fun follow(other: Position) = Position(x + (other.x - x).sign, y + (other.y - y).sign)
 }
 
-class Move(dir: String) {
+class Move(dir: Char) {
     val dirX: Int
     val dirY: Int
     init {
         when(dir) {
-            "U" -> {dirX = 0; dirY = 1}
-            "D" -> {dirX = 0; dirY = -1}
-            "L" -> {dirX = -1; dirY = 0}
-            "R" -> {dirX = 1; dirY = 0}
+            'U' -> {dirX = 0; dirY = 1}
+            'D' -> {dirX = 0; dirY = -1}
+            'L' -> {dirX = -1; dirY = 0}
+            'R' -> {dirX = 1; dirY = 0}
             else -> {dirX=0; dirY=0; println("IMPOSSIBLE!!")}
         }
     }
