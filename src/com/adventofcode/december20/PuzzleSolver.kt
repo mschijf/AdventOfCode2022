30d9363c29322bbbee3bbbd0bf0c51c5554ada44
mschijf <mschijf@gmail.com>
2022-12-20 21:43:45 +0100

    December 20 - refactoring

--- src/com/adventofcode/december20/PuzzleSolver.kt
+++ src/com/adventofcode/december20/PuzzleSolver.kt
@@ -4,37 +4,32 @@ import com.adventofcode.PuzzleSolverAbstract
 import java.lang.Math.floorMod
 
 fun main() {
-    PuzzleSolver(test=false).showResult()
+    PuzzleSolver(test=true).showResult()
 }
 
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
     override fun resultPartOne(): String {
-        val list = input.inputLines.map { Pair(it.toInt(), false) }.toMutableList()
+        val list = input.inputLines.mapIndexed { index, it -> Pair(it.toInt(), index) }.toMutableList()
 
-        var count =0
         val cycleLength = list.size
-        var orgIndex = list.indexOfFirst { !it.second }
-        while (orgIndex >= 0) {
+        for (index in list.indices) {
+            val orgIndex = list.indexOfFirst { it.second == index }
             val orgValue = list[orgIndex].first
-            val newPair = Pair(orgValue, true)
+            val newPair = Pair(orgValue, index)
             list.removeAt(orgIndex)
             val newIndex = orgIndex + orgValue
             if (newIndex < 0) {
-//                list.add(floorMod(newIndex, cycleLength) - ((-newIndex / cycleLength)+1) , newPair)
                 list.add(floorMod(newIndex, cycleLength-1), newPair)
             } else if (newIndex > cycleLength) {
-//                list.add(floorMod(newIndex, cycleLength) + (newIndex / cycleLength), newPair)
                 list.add(floorMod(newIndex, cycleLength-1), newPair)
             }  else if (newIndex == 0) {
                 list.add(newPair)
             } else {
                 list.add(newIndex, newPair)
             }
-            orgIndex = list.indexOfFirst { !it.second }
-            count++
         }
-        println("Count $count:     ${list.map { it.first }}")
+        println("${list.map { it.first }}")
 
         val index0 = list.indexOfFirst { it.first == 0 }
         val value1000 = list[(index0 + 1000) % cycleLength].first
@@ -47,43 +42,29 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
     override fun resultPartTwo(): String {
         val decryptionKey = 811589153L
-        var list = input.inputLines.mapIndexed { index, it -> Pair(it.toLong()*decryptionKey, index) }.toMutableList()
+        val list = input.inputLines.mapIndexed { index, it -> Pair(it.toLong()*decryptionKey, index) }.toMutableList()
         val cycleLength = list.size
 
-
         repeat(10) {
-            var index = 0
-            var orgIndex = list.indexOfFirst { it.second == index}
-            while (orgIndex >= 0) {
+            for (index in list.indices) {
+                val orgIndex = list.indexOfFirst { it.second == index}
                 val orgValue = list[orgIndex].first
                 val newPair = Pair(orgValue, index)
 
-                if (orgValue == 0L) {
-                    list[orgIndex] = newPair
+                list.removeAt(orgIndex)
+
+                val newIndex = orgIndex + orgValue
+                if (newIndex < 0) {
+                    list.add(floorMod(newIndex, cycleLength - 1), newPair)
+                } else if (newIndex > cycleLength) {
+                    list.add(floorMod(newIndex, cycleLength - 1), newPair)
+                } else if (newIndex == 0L) {
+                    list.add(newPair)
                 } else {
-
-                    list.removeAt(orgIndex)
-
-                    val newIndex = orgIndex + orgValue
-                    if (newIndex < 0) {
-                        list.add(floorMod(newIndex, cycleLength - 1), newPair)
-                    } else if (newIndex > cycleLength) {
-                        list.add(floorMod(newIndex, cycleLength - 1), newPair)
-                    } else if (newIndex == 0L) {
-                        list.add(newPair)
-                    } else {
-                        list.add(newIndex.toInt(), newPair)
-                    }
+                    list.add(newIndex.toInt(), newPair)
                 }
-                index++
-                orgIndex = list.indexOfFirst { it.second == index }
             }
-
-//            for (i in list.indices) {
-//                list[i] = Pair(list[i].first, false)
-//            }
         }
-
 
         val index0 = list.indexOfFirst { it.first == 0L }
         val value1000 = list[(index0 + 1000) % cycleLength].first
@@ -93,7 +74,6 @@ class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
         return (value1000 + value2000 + value3000).toString()
     }
-
 
 }
 
