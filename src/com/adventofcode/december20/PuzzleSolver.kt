28b6b815950bda9995457666b03008d804e663cc
mschijf <mschijf@gmail.com>
2022-12-20 21:52:43 +0100

    December 20 - more refactoring, more methods

--- src/com/adventofcode/december20/PuzzleSolver.kt
+++ src/com/adventofcode/december20/PuzzleSolver.kt
@@ -4,76 +4,60 @@ import com.adventofcode.PuzzleSolverAbstract
 import java.lang.Math.floorMod
 
 fun main() {
-    PuzzleSolver(test=true).showResult()
+    PuzzleSolver(test=false).showResult()
 }
 
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
 
     override fun resultPartOne(): String {
-        val list = input.inputLines.mapIndexed { index, it -> Pair(it.toInt(), index) }.toMutableList()
+        val list = input.inputLines.mapIndexed { index, it -> Pair(it.toLong(), index) }.toMutableList()
 
-        val cycleLength = list.size
-        for (index in list.indices) {
-            val orgIndex = list.indexOfFirst { it.second == index }
-            val orgValue = list[orgIndex].first
-            val newPair = Pair(orgValue, index)
-            list.removeAt(orgIndex)
-            val newIndex = orgIndex + orgValue
-            if (newIndex < 0) {
-                list.add(floorMod(newIndex, cycleLength-1), newPair)
-            } else if (newIndex > cycleLength) {
-                list.add(floorMod(newIndex, cycleLength-1), newPair)
-            }  else if (newIndex == 0) {
-                list.add(newPair)
-            } else {
-                list.add(newIndex, newPair)
-            }
-        }
-        println("${list.map { it.first }}")
-
-        val index0 = list.indexOfFirst { it.first == 0 }
-        val value1000 = list[(index0 + 1000) % cycleLength].first
-        val value2000 = list[(index0 + 2000) % cycleLength].first
-        val value3000 = list[(index0 + 3000) % cycleLength].first
-
-
-        return (value1000 + value2000 + value3000).toString()
+        mixList(list)
+        return sumOfThreeValues(list).toString()
     }
 
     override fun resultPartTwo(): String {
         val decryptionKey = 811589153L
         val list = input.inputLines.mapIndexed { index, it -> Pair(it.toLong()*decryptionKey, index) }.toMutableList()
-        val cycleLength = list.size
 
         repeat(10) {
-            for (index in list.indices) {
-                val orgIndex = list.indexOfFirst { it.second == index}
-                val orgValue = list[orgIndex].first
-                val newPair = Pair(orgValue, index)
+           mixList(list)
+        }
+        return sumOfThreeValues(list).toString()
+    }
 
-                list.removeAt(orgIndex)
 
-                val newIndex = orgIndex + orgValue
-                if (newIndex < 0) {
-                    list.add(floorMod(newIndex, cycleLength - 1), newPair)
-                } else if (newIndex > cycleLength) {
-                    list.add(floorMod(newIndex, cycleLength - 1), newPair)
-                } else if (newIndex == 0L) {
-                    list.add(newPair)
-                } else {
-                    list.add(newIndex.toInt(), newPair)
-                }
+    private fun mixList(list: MutableList<Pair<Long, Int>>) {
+        val cycleLength = list.size
+        for (index in list.indices) {
+            val orgIndex = list.indexOfFirst { it.second == index}
+            val orgValue = list[orgIndex].first
+            val newPair = Pair(orgValue, index)
+
+            list.removeAt(orgIndex)
+
+            val newIndex = orgIndex + orgValue
+            if (newIndex < 0) {
+                list.add(floorMod(newIndex, cycleLength - 1), newPair)
+            } else if (newIndex > cycleLength) {
+                list.add(floorMod(newIndex, cycleLength - 1), newPair)
+            } else if (newIndex == 0L) {
+                list.add(newPair)
+            } else {
+                list.add(newIndex.toInt(), newPair)
             }
         }
+    }
 
+    private fun sumOfThreeValues(list: MutableList<Pair<Long, Int>>): Long {
+        val cycleLength = list.size
         val index0 = list.indexOfFirst { it.first == 0L }
         val value1000 = list[(index0 + 1000) % cycleLength].first
         val value2000 = list[(index0 + 2000) % cycleLength].first
         val value3000 = list[(index0 + 3000) % cycleLength].first
-
-
-        return (value1000 + value2000 + value3000).toString()
+        return (value1000 + value2000 + value3000)
     }
+
 
 }
 
