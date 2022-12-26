dcbd8a5840c9a15fec7478760e14509d1a4ab6c6
mschijf <mschijf@gmail.com>
2022-12-26 20:55:03 +0100

    December 19 - first part 1 and part 2 solution

--- src/com/adventofcode/december19/PuzzleSolver.kt
+++ src/com/adventofcode/december19/PuzzleSolver.kt
@@ -1,6 +1,7 @@
 package com.adventofcode.december19
 
 import com.adventofcode.PuzzleSolverAbstract
+import java.lang.Integer.max
 import java.lang.System.currentTimeMillis
 
 fun main() {
@@ -8,83 +9,137 @@ fun main() {
 }
 
 class PuzzleSolver(test: Boolean) : PuzzleSolverAbstract(test) {
-    val bluePrintList = input.inputLines.map { BluePrint(it) }
+    private val bluePrintList = input.inputLines.map { BluePrint(it) }
 
-    val cache = Array(25) {HashMap<String, Int>()}
+    private val cache = Array(33) {HashMap<String, Int>()}
 
-    override fun resultPartOne(): String {
-        var quality = 0
-        for (bluePrint in bluePrintList) {
+//    override fun resultPartOne(): String {
+//        var quality = 0
+//        for (bluePrint in bluePrintList) {
+//            val startTime = currentTimeMillis()
+//            val executor = Executor(bluePrint)
+//            print("Start Blueprint ${bluePrint.number} of ${bluePrintList.size}")
+//
+//            cache.forEach { it.clear() }
+//            val gc = solver(executor, 24, -1, emptyList())
+//            val timePassed = currentTimeMillis() - startTime
+//            println(" --> $gc time: $timePassed ms")
+//
+//            quality += (gc.geodeCollected*bluePrint.number)
+//        }
+//        return quality.toString()
+//    }
+
+    //10064 --> too low
+
+    override fun resultPartTwo(): String {
+        var quality = 1
+        for (bluePrint in bluePrintList.dropLast(max(0, bluePrintList.size-3))) {
             val startTime = currentTimeMillis()
             val executor = Executor(bluePrint)
             print("Start Blueprint ${bluePrint.number} of ${bluePrintList.size}")
 
             cache.forEach { it.clear() }
-
-            val gc = solver(executor, 24)
+            val gc = solver(executor, 32, -1, emptyList())
             val timePassed = currentTimeMillis() - startTime
             println(" --> $gc time: $timePassed ms")
-            quality += (gc*bluePrint.number)
+
+//            val startTime2 = currentTimeMillis()
+//            val executor2 = Executor(bluePrint)
+//            cache.forEach { it.clear() }
+//            print("     nu tot 32: ${bluePrint.number} of ${bluePrintList.size}")
+//            val gc2 = solver(executor2, 32, gc.geodeCollected, gc.path)
+//            val timePassed2 = currentTimeMillis() - startTime2
+//            println(" --> $gc2 time: $timePassed2 ms")
+
+            quality *= (gc.geodeCollected)
         }
         return quality.toString()
     }
 
-    private fun solver(executor: Executor, minutesLeft: Int): Int {
+    private fun solver(executor: Executor, minutesLeft: Int, maxToReach: Int, suggestedActionPath: List<RobotType>): SearchInfo {
         if (minutesLeft <= 0) {
-            return executor.geodeCount
+            return SearchInfo(executor.geodeCount, emptyList())
+        }
+
+        val maxPotentialGeode = (minutesLeft * (minutesLeft-1))/2
+        if (executor.geodeCount + executor.geodeRobotCount * minutesLeft + maxPotentialGeode <= maxToReach) {
+            return SearchInfo(-1, emptyList())
         }
 
         if (cache[minutesLeft].contains(executor.hashString())) {
-            return cache[minutesLeft][executor.hashString()] ?: 0
+            return SearchInfo(cache[minutesLeft][executor.hashString()] ?: -1, emptyList())
         }
 
-        if (minutesLeft == 2) {
-            if (!executor.canBeMade(RobotType.GEODE)) {
-                return executor.geodeCount + minutesLeft*executor.geodeRobotCount
-            } else {
-                return executor.geodeCount + minutesLeft*executor.geodeRobotCount + 1
+
+//        if (minutesLeft == 2) {
+//            if (!executor.canBeMade(RobotType.GEODE)) {
+//                return SearchInfo(executor.geodeCount + minutesLeft*executor.geodeRobotCount,
+//                    listOf(RobotType.NONE, RobotType.NONE))
+//            } else {
+//                return SearchInfo(executor.geodeCount + minutesLeft*executor.geodeRobotCount + 1,
+//                    listOf(RobotType.NONE, RobotType.GEODE))
+//            }
+//        }
+//
+//        if (executor.canBeMade(RobotType.GEODE)) {
+//            executor.doAction(RobotType.GEODE)
+//            val searchResult = solver(executor, minutesLeft - 1, maxToReach, suggestedActionPath.drop(1))
+//            cache[minutesLeft][executor.hashString()] = searchResult.geodeCollected
+//            executor.undoAction(RobotType.GEODE)
+//            return SearchInfo(searchResult.geodeCollected, listOf(RobotType.GEODE) + searchResult.path)
+//        }
+
+        var bestResult = SearchInfo(-1, emptyList())
+        val actionList = executor.generateActionList(suggestedActionPath.firstOrNull()?:RobotType.GEODE)
+        for (robotType in actionList) {
+            executor.doAction(robotType)
+            val searchResult = solver(executor, minutesLeft - 1, max(maxToReach, bestResult.geodeCollected), suggestedActionPath.drop(1))
+            if (searchResult.better(bestResult)) {
+                bestResult = SearchInfo(searchResult.geodeCollected, listOf(robotType) + searchResult.path)
             }
+            executor.undoAction(robotType)
         }
+        cache[minutesLeft][executor.hashString()] = bestResult.geodeCollected
 
-        if (executor.canBeMade(RobotType.GEODE)) {
-            executor.doAction(RobotType.GEODE)
-            val geodeCollected = solver(executor, minutesLeft - 1)
-            cache[minutesLeft][executor.hashString()] = geodeCollected
-            executor.undoAction(RobotType.GEODE)
-            return geodeCollected
-        }
-
-        var maxGeodeCollected = 0
-        for (robotType in RobotType.values()) {
-            if (executor.canBeMade(robotType)) {
-                executor.doAction(robotType)
-                val geodeCollected = solver(executor, minutesLeft - 1)
-                cache[minutesLeft][executor.hashString()] = geodeCollected
-                if (geodeCollected > maxGeodeCollected) {
-                    maxGeodeCollected = geodeCollected
-                }
-                executor.undoAction(robotType)
-            }
-        }
-
-        return maxGeodeCollected
+        return bestResult
     }
+}
+
+class SearchInfo(val geodeCollected: Int, val path: List<RobotType>) {
+    fun better(other: SearchInfo): Boolean = geodeCollected > other.geodeCollected
+//    override fun toString() = "$geodeCollected \n  $path"
+    override fun toString() = "$geodeCollected"
 }
 
 //----------------------------------------------------------------------------------------------------------------------
 
-class Executor(val bluePrint: BluePrint) {
-    var oreRobotCount = 1
-    var clayRobotCount = 0
-    var obsidianRobotCount = 0
+class Executor(private val bluePrint: BluePrint) {
+    private var oreRobotCount = 1
+    private var clayRobotCount = 0
+    private var obsidianRobotCount = 0
     var geodeRobotCount = 0
 
-    var oreCount = 0
-    var clayCount = 0
-    var obsidianCount = 0
+    private var oreCount = 0
+    private var clayCount = 0
+    private var obsidianCount = 0
     var geodeCount = 0
 
     fun hashString() = "$oreRobotCount,$clayRobotCount,$obsidianRobotCount,$geodeRobotCount,$oreCount,$clayCount,$obsidianCount,$geodeCount"
+
+    fun generateActionList(suggestedAction: RobotType) : List<RobotType> {
+        var result = mutableListOf<RobotType>()
+        for (robotType in RobotType.values()) {
+            if (this.canBeMade(robotType)) {
+                if (robotType == suggestedAction) {
+                    result.add(0, robotType)
+                } else {
+                    result.add(robotType)
+                }
+            }
+        }
+        return result
+    }
 
     fun doAction(robotType: RobotType) {
         preMakeRobot(robotType)
